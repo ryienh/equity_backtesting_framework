0f73dd9c664d1ba4129c9a5d4cb73736464e9bd7
ryienh <ryienh@umich.edu>
2018-06-19 16:37:11 -0400

    Added second stock to example

--- example_algo.py
+++ example_algo.py
@@ -1,7 +1,12 @@
+'''
+Simple algorithm which demonstrates the use of several features in the
+backtesting framework
+'''
+
+#Import framework
 import market
 
 #Make a portfolio
-
 my_portfolio = market.Portfolio(10000)
 print(my_portfolio.get_stocks_owned())
 print(my_portfolio.get_start_cash())
@@ -9,11 +14,12 @@ print(my_portfolio.get_current_cash())
 print(my_portfolio.get_number_of_trades())
 print('\n')
 
-#Inititalize stock
+#Inititalize stocks
 aapl = market.Stock('aapl', '2013-01-01', '2018-06-19')
+snap = market.Stock('snap', '2013-01-01', '2018-06-19')
 
 #Buy stock
-my_portfolio.buy_stock_at_close(aapl, '2017-12-02', 12)
+my_portfolio.buy_stock_at_close(snap, '2017-12-02', 12)
 print(my_portfolio.get_stocks_owned())
 print(my_portfolio.get_start_cash())
 print(my_portfolio.get_current_cash())
@@ -28,7 +34,7 @@ print(my_portfolio.get_number_of_trades())
 print('\n')
 
 #Sell stock
-my_portfolio.sell_stock_at_close(aapl, '2018-03-04', 2)
+my_portfolio.sell_stock_at_close(snap, '2018-03-04', 2)
 print(my_portfolio.get_stocks_owned())
 print(my_portfolio.get_start_cash())
 print(my_portfolio.get_current_cash())
